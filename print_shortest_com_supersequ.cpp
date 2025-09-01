e40a1766ab53bb916ab7fa0fca869c6a6fe9ed2e
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2025-09-01 22:55:42 +0530

    print  shortset_comon_supersubsequence

--- print_shortest_com_supersequ.cpp
+++ print_shortest_com_supersequ.cpp
@@ -10,13 +10,19 @@ string str;
 
 // main content 
 
+for (int i = 0; i <= size_a; i++) {
+    for (int j = 0; j <= size_b; j++) {
+        if (i == 0 || j == 0)
+            t[i][j] = 0; 
+    }
+    }
+
  for (int i = 1; i <= size_a; i++)
     {
         for (int j = 1; j <= size_b; j++)
         {
-            if (i == 0 || j == 0)
-                t[i][j] = 0; // inslization
-            else if (a[i - 1] == b[j - 1])
+          
+            if (a[i - 1] == b[j - 1])
                 t[i][j] = 1 + t[i - 1][j - 1];
             else
                 t[i][j] = max(t[i - 1][j], t[i][j - 1]);
@@ -36,8 +42,8 @@ string str;
                 str.push_back(b[j-1]);
                 j--;
             }          
-            else if(t[i-1][j]>t[i][j-1]){
-                str.push_back(b[i-1]);
+            else{
+                str.push_back(a[i-1]);
                 i--;
             }
         }
